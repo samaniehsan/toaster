74abbf1da82ea83904f9af435f429b74f1176ea5
Ehsan Samani <ehsan.samani@outlook.com>
2021-02-25 15:00:39 -0600

    empty classes

---
+++ toaster/IToastable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace toaster
+{
+    public interface IToastable
+    {
+    }
+}
---
+++ toaster/Toaster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace toaster
+{
+    public class Toaster
+    {
+        public uint SlotCount { get; private set; }
+        public uint LeverCount { get; private set; }
+
+        private Dictionary<uint,  uint> LevelToSlotMapping { get; set; }
+
+        public void SettSlotItem(uint slot, Item item)
+        {
+
+        }
+        public void SetDuration(uint lever, uint duration)
+        {
+
+        }
+        public void Start(uint lever)
+        {
+
+        }
+        public void Stop(uint lever)
+        {
+
+        }
+        public void Stop()
+        {
+
+        }
+    }
+}
