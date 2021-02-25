49412f13616d7a8c3602927e938f6fde050631fa
Ehsan Samani <ehsan.samani@outlook.com>
2021-02-25 14:39:08 -0600

    initial commit

---
+++ toaster/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace toaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
